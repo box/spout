f47c00090175147d11f59a7349c859b4c20ec096
Grzegorz Daszuta <grzegorz.daszuta@codewave.pl>
2015-04-02 11:33:51 +0200

    Guess Excel cell type basing on array cell value type

---
+++ src/Spout/Writer/Exception/InvalidDataException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Box\Spout\Writer\Exception;
+
+/**
+ * Class InvalidDataException
+ *
+ * @package Box\Spout\Writer\Exception
+ */
+class InvalidDataException extends SpoutException
+{
+}
