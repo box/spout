7efab5576da2a813a32683b573f770a5e8983eea
Adrien Loison <aloison@box.com>
2015-08-21 15:21:35 -0700

    Detection of invalid sheet name
    Based on Excel requirements:
     - it should not be blank
     - it should not exceed 31 characters
     - it should not contain these characters: \ / ? * [ or ]
     - it should be unique

--- tests/Spout/Writer/XLSX/SheetTest.php
+++ tests/Spout/Writer/XLSX/SheetTest.php
@@ -20,7 +20,7 @@ class SheetTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetSheetIndex()
     {
-        $sheets = $this->writeDataAndReturnSheets('test_get_sheet_index.xlsx');
+        $sheets = $this->writeDataToMulitpleSheetsAndReturnSheets('test_get_sheet_index.xlsx');
 
         $this->assertEquals(2, count($sheets), '2 sheets should have been created');
         $this->assertEquals(0, $sheets[0]->getIndex(), 'The first sheet should be index 0');
@@ -32,7 +32,7 @@ class SheetTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetSheetName()
     {
-        $sheets = $this->writeDataAndReturnSheets('test_get_sheet_name.xlsx');
+        $sheets = $this->writeDataToMulitpleSheetsAndReturnSheets('test_get_sheet_name.xlsx');
 
         $this->assertEquals(2, count($sheets), '2 sheets should have been created');
         $this->assertEquals('Sheet1', $sheets[0]->getName(), 'Invalid name for the first sheet');
@@ -45,27 +45,115 @@ class SheetTest extends \PHPUnit_Framework_TestCase
     public function testSetSheetNameShouldCreateSheetWithCustomName()
     {
         $fileName = 'test_set_name_should_create_sheet_with_custom_name.xlsx';
+        $customSheetName = 'CustomName';
+        $this->writeDataAndReturnSheetWithCustomName($fileName, $customSheetName);
+
+        $this->assertSheetNameEquals($customSheetName, $fileName, "The sheet name should have been changed to '$customSheetName'");
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderForInvalidSheetNames()
+    {
+        return [
+            [null],
+            [21],
+            [''],
+            ['this title exceeds the 31 characters limit'],
+            ['Illegal \\'],
+            ['Illegal /'],
+            ['Illegal ?'],
+            ['Illegal *'],
+            ['Illegal ['],
+            ['Illegal ]'],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForInvalidSheetNames
+     * @expectedException \Box\Spout\Writer\Exception\InvalidSheetNameException
+     *
+     * @param string $customSheetName
+     * @return void
+     */
+    public function testSetSheetNameShouldThrowOnInvalidName($customSheetName)
+    {
+        $fileName = 'test_set_name_with_invalid_name_should_throw_exception.xlsx';
+        $this->writeDataAndReturnSheetWithCustomName($fileName, $customSheetName);
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetSheetNameShouldNotThrowWhenSettingSameNameAsCurrentOne()
+    {
+        $fileName = 'test_set_name_with_same_as_current.xlsx';
         $this->createGeneratedFolderIfNeeded($fileName);
         $resourcePath = $this->getGeneratedResourcePath($fileName);
 
         $writer = WriterFactory::create(Type::XLSX);
         $writer->openToFile($resourcePath);
 
-        $customSheetName = 'CustomName';
+        $customSheetName = 'Sheet name';
         $sheet = $writer->getCurrentSheet();
+        $sheet->setName($customSheetName);
         $sheet->setName($customSheetName);
 
         $writer->addRow(['xlsx--11', 'xlsx--12']);
         $writer->close();
 
-        $this->assertSheetNameEquals($customSheetName, $resourcePath, "The sheet name should have been changed to '$customSheetName'");
+        $this->assertSheetNameEquals($customSheetName, $fileName, "The sheet name should have been changed to '$customSheetName'");
+    }
+
+    /**
+     * @expectedException \Box\Spout\Writer\Exception\InvalidSheetNameException
+     * @return void
+     */
+    public function testSetSheetNameShouldThrowWhenNameIsAlreadyUsed()
+    {
+        $fileName = 'test_set_name_with_non_unique_name.xlsx';
+        $this->createGeneratedFolderIfNeeded($fileName);
+        $resourcePath = $this->getGeneratedResourcePath($fileName);
+
+        $writer = WriterFactory::create(Type::XLSX);
+        $writer->openToFile($resourcePath);
+
+        $customSheetName = 'Sheet name';
+
+        $sheet = $writer->getCurrentSheet();
+        $sheet->setName($customSheetName);
+
+        $writer->addNewSheetAndMakeItCurrent();
+        $sheet = $writer->getCurrentSheet();
+        $sheet->setName($customSheetName);
+    }
+
+    /**
+     * @param string $fileName
+     * @param string $sheetName
+     * @return Sheet
+     */
+    private function writeDataAndReturnSheetWithCustomName($fileName, $sheetName)
+    {
+        $this->createGeneratedFolderIfNeeded($fileName);
+        $resourcePath = $this->getGeneratedResourcePath($fileName);
+
+        $writer = WriterFactory::create(Type::XLSX);
+        $writer->openToFile($resourcePath);
+
+        $sheet = $writer->getCurrentSheet();
+        $sheet->setName($sheetName);
+
+        $writer->addRow(['xlsx--11', 'xlsx--12']);
+        $writer->close();
     }
 
     /**
      * @param string $fileName
      * @return Sheet[]
      */
-    private function writeDataAndReturnSheets($fileName)
+    private function writeDataToMulitpleSheetsAndReturnSheets($fileName)
     {
         $this->createGeneratedFolderIfNeeded($fileName);
         $resourcePath = $this->getGeneratedResourcePath($fileName);
@@ -85,12 +173,13 @@ class SheetTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @param string $expectedName
-     * @param string $resourcePath
+     * @param string $fileName
      * @param string $message
      * @return void
      */
-    private function assertSheetNameEquals($expectedName, $resourcePath, $message = '')
+    private function assertSheetNameEquals($expectedName, $fileName, $message = '')
     {
+        $resourcePath = $this->getGeneratedResourcePath($fileName);
         $pathToWorkbookFile = $resourcePath . '#xl/workbook.xml';
         $xmlContents = file_get_contents('zip://' . $pathToWorkbookFile);
 
