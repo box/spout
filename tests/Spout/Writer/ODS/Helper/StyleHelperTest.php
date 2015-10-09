976eff514e08fd9167166a50c346835702c82c22
Jan <jan@ingoedebanen.nl>
2015-10-09 14:05:19 +0200

    Support for style per column for XLSX and ODS - test with styles array

--- tests/Spout/Writer/ODS/Helper/StyleHelperTest.php
+++ tests/Spout/Writer/ODS/Helper/StyleHelperTest.php
@@ -50,11 +50,11 @@ class StyleHelperTest extends \PHPUnit_Framework_TestCase
         $style = (new StyleBuilder())->setFontBold()->build();
 
         $styleHelper = new StyleHelper($this->defaultStyle);
-        $registeredStyle1 = $styleHelper->registerStyle($style);
-        $registeredStyle2 = $styleHelper->registerStyle($style);
+        $registeredStyles = $styleHelper->registerStyle($style);
+        $registeredStyles = $styleHelper->registerStyle($style);
 
-        $this->assertEquals(1, $registeredStyle1->getId());
-        $this->assertEquals(1, $registeredStyle2->getId());
+        $this->assertEquals(1, $registeredStyles[0]->getId());
+        $this->assertEquals(1, $registeredStyles[0]->getId());
     }
 
     /**
