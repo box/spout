643d1e102f443db68be9df4c499891b54efe6e7a
Adrien Loison <adrilo@users.noreply.github.com>
2016-06-07 19:09:10 -0700

    Clear previous output when openToBrowser() called
    If any character has been outputted before, the generated file will be corrupted.

--- src/Spout/Common/Helper/GlobalFunctionsHelper.php
+++ src/Spout/Common/Helper/GlobalFunctionsHelper.php
@@ -253,6 +253,17 @@ class GlobalFunctionsHelper
     }
 
     /**
+     * Wrapper around global function ob_end_clean()
+     * @see ob_end_clean()
+     *
+     * @return void
+     */
+    public function ob_end_clean()
+    {
+        ob_end_clean();
+    }
+
+    /**
      * Wrapper around global function iconv()
      * @see iconv()
      *
