6f4ddb15696fd39226b0852ec522bca2de250a72
Lito <lito@eordes.com>
2017-01-04 18:50:12 +0100

    Fixed processDimensionStartingNode regular expression (#372)
    * Fixed processDimensionStartingNode regular expression
    * Improved processDimensionStartingNode regular expression
    * Removed strict control on processDimensionStartingNode regular expression

--- src/Spout/Reader/XLSX/RowIterator.php
+++ src/Spout/Reader/XLSX/RowIterator.php
@@ -220,9 +220,8 @@ class RowIterator implements IteratorInterface
     {
         // Read dimensions of the sheet
         $dimensionRef = $xmlReader->getAttribute(self::XML_ATTRIBUTE_REF); // returns 'A1:M13' for instance (or 'A1' for empty sheet)
-        if (preg_match('/[A-Z\d]+:([A-Z\d]+)/', $dimensionRef, $matches)) {
-            $lastCellIndex = $matches[1];
-            $this->numColumns = CellHelper::getColumnIndexFromCellIndex($lastCellIndex) + 1;
+        if (preg_match('/[A-Z]+\d+:([A-Z]+\d+)/', $dimensionRef, $matches)) {
+            $this->numColumns = CellHelper::getColumnIndexFromCellIndex($matches[1]) + 1;
         }
 
         return XMLProcessor::PROCESSING_CONTINUE;
