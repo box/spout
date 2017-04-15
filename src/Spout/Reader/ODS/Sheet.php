7f8b95b2f32166df8a2dc18064ca2e1ad8e1812c
Adrien Loison <adrilo@users.noreply.github.com>
2017-04-15 21:40:19 +0200

    Expose Sheet::isActive() to provide info about the last active sheet (#405)

--- src/Spout/Reader/ODS/Sheet.php
+++ src/Spout/Reader/ODS/Sheet.php
@@ -25,17 +25,22 @@ class Sheet implements SheetInterface
     /** @var string Name of the sheet */
     protected $name;
 
+    /** @var bool Whether the sheet was the active one */
+    protected $isActive;
+
     /**
      * @param XMLReader $xmlReader XML Reader, positioned on the "<table:table>" element
      * @param int $sheetIndex Index of the sheet, based on order in the workbook (zero-based)
-     * @param \Box\Spout\Reader\ODS\ReaderOptions $options Reader's current options
      * @param string $sheetName Name of the sheet
+     * @param bool $isSheetActive Whether the sheet was defined as active
+     * @param \Box\Spout\Reader\ODS\ReaderOptions $options Reader's current options
      */
-    public function __construct($xmlReader, $sheetIndex, $sheetName, $options)
+    public function __construct($xmlReader, $sheetIndex, $sheetName, $isSheetActive, $options)
     {
         $this->rowIterator = new RowIterator($xmlReader, $options);
         $this->index = $sheetIndex;
         $this->name = $sheetName;
+        $this->isActive = $isSheetActive;
     }
 
     /**
@@ -63,5 +68,14 @@ class Sheet implements SheetInterface
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * @api
+     * @return bool Whether the sheet was defined as active
+     */
+    public function isActive()
+    {
+        return $this->isActive;
     }
 }
--- tests/Spout/Reader/ODS/SheetTest.php
+++ tests/Spout/Reader/ODS/SheetTest.php
@@ -18,15 +18,30 @@ class SheetTest extends \PHPUnit_Framework_TestCase
     /**
      * @return void
      */
-    public function testNextSheetShouldReturnCorrectSheetInfos()
+    public function testReaderShouldReturnCorrectSheetInfos()
     {
+        // NOTE: This spreadsheet has its second tab defined as active
         $sheets = $this->openFileAndReturnSheets('two_sheets_with_custom_names.ods');
 
         $this->assertEquals('Sheet First', $sheets[0]->getName());
         $this->assertEquals(0, $sheets[0]->getIndex());
+        $this->assertFalse($sheets[0]->isActive());
 
         $this->assertEquals('Sheet Last', $sheets[1]->getName());
         $this->assertEquals(1, $sheets[1]->getIndex());
+        $this->assertTrue($sheets[1]->isActive());
+    }
+
+    /**
+     * @return void
+     */
+    public function testReaderShouldDefineFirstSheetAsActiveByDefault()
+    {
+        // NOTE: This spreadsheet has no information about the active sheet
+        $sheets = $this->openFileAndReturnSheets('two_sheets_with_no_settings_xml_file.ods');
+
+        $this->assertTrue($sheets[0]->isActive());
+        $this->assertFalse($sheets[1]->isActive());
     }
 
     /**
