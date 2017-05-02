77779555773da879a8e04e5ba85da72d9f782a3c
Adrien Loison <adrilo@users.noreply.github.com>
2017-05-02 14:11:52 +0200

    First commit to gh-pages branch

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(dirname(__DIR__) . '/vendor/autoload.php');
-
-require_once(dirname(__DIR__) . '/tests/Spout/TestUsingResource.php');
-require_once(dirname(__DIR__) . '/tests/Spout/ReflectionHelper.php');
-require_once(dirname(__DIR__) . '/tests/Spout/Reader/CSV/SpoutTestStream.php');
-
-// Make sure a timezone is set to be able to work with dates
-date_default_timezone_set('UTC');
