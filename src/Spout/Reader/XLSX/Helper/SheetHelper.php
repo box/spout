68ff1b7b80a15894ff1702128bf4df831e0373e9
alexisparron <alexis.parron@saleslayer.com>
2017-07-05 13:45:30 +0200

    Correction Status by State and make visible a constant

--- src/Spout/Reader/XLSX/Helper/SheetHelper.php
+++ src/Spout/Reader/XLSX/Helper/SheetHelper.php
@@ -27,9 +27,11 @@ class SheetHelper
     const XML_ATTRIBUTE_ACTIVE_TAB = 'activeTab';
     const XML_ATTRIBUTE_R_ID = 'r:id';
     const XML_ATTRIBUTE_NAME = 'name';
-    const XML_ATTRIBUTE_STATUS = 'status';
+    const XML_ATTRIBUTE_STATE = 'state';
     const XML_ATTRIBUTE_ID = 'Id';
     const XML_ATTRIBUTE_TARGET = 'Target';
+    
+    const SHEET_STATUS_VISIBLE = 'visible';
 
     /** @var string Path of the XLSX file being read */
     protected $filePath;
@@ -106,12 +108,12 @@ class SheetHelper
     {
         $sheetId = $xmlReaderOnSheetNode->getAttribute(self::XML_ATTRIBUTE_R_ID);
         $escapedSheetName = $xmlReaderOnSheetNode->getAttribute(self::XML_ATTRIBUTE_NAME);
-        $sheetStatus = $xmlReaderOnSheetNode->getAttribute(self::XML_ATTRIBUTE_STATUS);
+        $sheetState = $xmlReaderOnSheetNode->getAttribute(self::XML_ATTRIBUTE_STATE);
 
         /** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
         $escaper = \Box\Spout\Common\Escaper\XLSX::getInstance();
         $sheetName = $escaper->unescape($escapedSheetName);
-        $sheetVisible = ($sheetStatus == 'visible') ? true : false;
+        $sheetVisible = ($sheetState === self::SHEET_STATUS_VISIBLE) ? true : false;
 
         $sheetDataXMLFilePath = $this->getSheetDataXMLFilePathForSheetId($sheetId);
 
