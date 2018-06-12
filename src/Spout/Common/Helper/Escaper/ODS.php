31b8e50c25bbc81cae9e689715112c7f22a53ec2
madflow <madflow@web.de>
2018-06-12 07:53:22 +0200

    remove HHVM workarounds

--- src/Spout/Common/Helper/Escaper/ODS.php
+++ src/Spout/Common/Helper/Escaper/ODS.php
@@ -9,40 +9,22 @@ namespace Box\Spout\Common\Helper\Escaper;
 class ODS implements EscaperInterface
 {
     /**
-     * Escapes the given string to make it compatible with XLSX
+     * Escapes the given string to make it compatible with ODS
      *
      * @param string $string The string to escape
      * @return string The escaped string
      */
     public function escape($string)
     {
-        if (defined('ENT_DISALLOWED')) {
-            // 'ENT_DISALLOWED' ensures that invalid characters in the given document type are replaced.
-            // Otherwise control characters like a vertical tab "\v" will make the XML document unreadable by the XML processor
-            // @link https://github.com/box/spout/issues/329
-            $replacedString = htmlspecialchars($string, ENT_NOQUOTES | ENT_DISALLOWED);
-        } else {
-            // We are on hhvm or any other engine that does not support ENT_DISALLOWED.
-            //
-            // @NOTE: Using ENT_NOQUOTES as only XML entities ('<', '>', '&') need to be encoded.
-            //        Single and double quotes can be left as is.
-            $escapedString =  htmlspecialchars($string, ENT_NOQUOTES);
-
-            // control characters values are from 0 to 1F (hex values) in the ASCII table
-            // some characters should not be escaped though: "\t", "\r" and "\n".
-            $regexPattern = '[\x00-\x08' .
-                            // skipping "\t" (0x9) and "\n" (0xA)
-                            '\x0B-\x0C' .
-                            // skipping "\r" (0xD)
-                            '\x0E-\x1F]';
-            $replacedString = preg_replace("/$regexPattern/", '�', $escapedString);
-        }
-
-        return $replacedString;
+        // 'ENT_DISALLOWED' ensures that invalid characters in the given document type are replaced.
+        // Otherwise control characters like a vertical tab "\v" will make the XML document unreadable
+        // by the XML processor
+        // @link https://github.com/box/spout/issues/329
+        return htmlspecialchars($string, ENT_NOQUOTES | ENT_DISALLOWED);
     }
 
     /**
-     * Unescapes the given string to make it compatible with XLSX
+     * Unescapes the given string to make it compatible with ODS
      *
      * @param string $string The string to unescape
      * @return string The unescaped string
