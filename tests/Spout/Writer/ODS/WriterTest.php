c68f4a2cfa652e246dd3e6401fa7f6b727b4441e
madflow <madflow@web.de>
2018-06-12 08:11:08 +0200

    some migrations to PHP 7.1

--- tests/Spout/Writer/ODS/WriterTest.php
+++ tests/Spout/Writer/ODS/WriterTest.php
@@ -10,7 +10,6 @@ use Box\Spout\Common\Type;
 use Box\Spout\Reader\Wrapper\XMLReader;
 use Box\Spout\TestUsingResource;
 use Box\Spout\Writer\Common\Creator\EntityFactory;
-use Box\Spout\Writer\Common\Helper\ZipHelper;
 use Box\Spout\Writer\Exception\WriterAlreadyOpenedException;
 use Box\Spout\Writer\Exception\WriterNotOpenedException;
 use Box\Spout\Writer\RowCreationHelper;
@@ -465,15 +464,6 @@ class WriterTest extends TestCase
      */
     public function testGeneratedFileShouldHaveTheCorrectMimeType()
     {
-        // Only PHP7+ gives the correct mime type since it requires adding
-        // uncompressed files to the final archive (which support was added in PHP7)
-        if (!ZipHelper::canChooseCompressionMethod()) {
-            $this->markTestSkipped(
-                'The PHP version used does not support setting the compression method of archived files,
-                resulting in the mime type to be detected incorrectly.'
-            );
-        }
-
         $fileName = 'test_mime_type.ods';
         $resourcePath = $this->getGeneratedResourcePath($fileName);
         $dataRow = ['foo'];
