0345b369c74025ad898270dbd984e75675fa35df
Adrien Loison <adrilo@users.noreply.github.com>
2021-11-12 15:00:14 +0100

    Fix tests with no locales + Apply CSFixer fixes

--- tests/Spout/Writer/XLSX/WriterTest.php
+++ tests/Spout/Writer/XLSX/WriterTest.php
@@ -406,12 +406,19 @@ class WriterTest extends TestCase
             // Installed locales differ from one system to another, so we can't pick
             // a given locale.
             $supportedLocales = explode("\n", shell_exec('locale -a'));
+            $foundCommaLocale = false;
             foreach ($supportedLocales as $supportedLocale) {
                 \setlocale(LC_ALL, $supportedLocale);
                 if (\localeconv()['decimal_point'] === ',') {
+                    $foundCommaLocale = true;
                     break;
                 }
             }
+
+            if (!$foundCommaLocale) {
+                $this->markTestSkipped('No locale with comma decimal separator');
+            }
+
             $this->assertEquals(',', \localeconv()['decimal_point']);
 
             $fileName = 'test_add_row_should_support_float_values_in_different_locale.xlsx';
@@ -421,8 +428,8 @@ class WriterTest extends TestCase
 
             $this->writeToXLSXFile($dataRows, $fileName, $shouldUseInlineStrings = false);
 
-            $this->assertInlineDataWasNotWrittenToSheet($fileName, 1, "1234,5");
-            $this->assertInlineDataWasWrittenToSheet($fileName, 1, "1234.5");
+            $this->assertInlineDataWasNotWrittenToSheet($fileName, 1, '1234,5');
+            $this->assertInlineDataWasWrittenToSheet($fileName, 1, '1234.5');
         } finally {
             // reset locale
             \setlocale(LC_ALL, $previousLocale);
